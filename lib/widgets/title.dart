5ffd36039d877e6b1f8e28687d240be186897df2
SungminPark1997 <72503832+SungminPark1997@users.noreply.github.com>
2024-08-09 16:46:04 +0900

    modify : title divider position

--- lib/widgets/title.dart
+++ lib/widgets/title.dart
@@ -7,27 +7,25 @@ class TitleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const SizedBox(
-          width: 30,
+        Row(
+          children: [
+            const SizedBox(
+              width: 30, // 텍스트 왼쪽의 공백
+            ),
+            Text(
+              title,
+              style: const TextStyle(color: Colors.white, fontSize: 40),
+            ),
+          ],
         ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // 좌측 정렬
-            children: [
-              Text(
-                title,
-                style: const TextStyle(color: Colors.white, fontSize: 40),
-              ),
-              const SizedBox(height: 5), // 텍스트와 경계선 사이의 간격
-              const Divider(
-                color: Colors.white, // 경계선의 색상
-                thickness: 2, // 경계선의 두께
-              ),
-            ],
-          ),
-        )
+        const SizedBox(height: 5), // 텍스트와 경계선 사이의 간격
+        const Divider(
+          color: Colors.white, // 경계선의 색상
+          thickness: 2, // 경계선의 두께
+        ),
       ],
     );
   }
